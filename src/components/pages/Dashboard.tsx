96f511c2fe0a9c5acff52c5926e5614c1edf3f33
imane-elk <117850683+imane-elk@users.noreply.github.com>
2025-08-27 14:32:32 +0100

    Update Dashboard.tsx

--- src/components/pages/Dashboard.tsx
+++ src/components/pages/Dashboard.tsx
@@ -1,7 +1,7 @@
 export default function Dashboard() {
   return (
     <div className='p-6'>
-      <h1 className='mb-6 text-3xl'>Bienvenue Imane </h1>
+      <h1 className='mb-6 text-3xl'>Bienvenue  </h1>
       <div className='grid grid-cols-3 gap-4'>
         <div className='rounded border p-4 shadow'>Stat 1</div>
         <div className='rounded border p-4 shadow'>Stat 2</div>
